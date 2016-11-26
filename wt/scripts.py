09c071ee6af51bed33cb077c59dfbe3db556c006
tertychnyy <itertychnyy@gmail.com>
2016-11-26 21:52:08 +0200

    added scenario logic

--- wt/scripts.py
+++ wt/scripts.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+import re
+
 from chatfirst.client import Chatfirst
 from chatfirst.models import Bot
 from flask import current_app
+from lxml import html, etree
+from lxml.etree import tostring
 
 from wt.core import InvalidUsage
 
@@ -32,8 +37,90 @@ def read_scenarion_file(r):
     return scenario
 
 
+def get_state_name(s, sfx):
+    return "State" + s + sfx
+
+
+def get_state_intro_name(s):
+    return get_state_name(s, "Intro")
+
+
+def get_state_menu_name(s):
+    return get_state_name(s, "Menu")
+
+
 def translate_raw_scenario(raw):
-    res = '<states><state name="Start"><transition input="start" next="start">It works better!</transition></state></states>'
+    root = html.fromstring(raw)
+
+    res_xml = etree.Element('states')
+    start_state = etree.Element('state')
+    start_state.attrib['name'] = 'Start'
+    start_tr = etree.Element('transition')
+    start_tr.attrib['input'] = '*'
+    start_tr.attrib['next'] = 'Start'
+    start_tr.text = "Please, add Start passage"
+
+    res_xml.append(start_state)
+
+    for passage in root.findall("tw-passagedata"):
+        name = passage.attrib["name"]
+        text = passage.text
+
+        # Find special data
+        keys_re = r"\[\[[^\[\]]*\]\]"
+        keys = [x[2:-2] for x in re.findall(keys_re, passage.text)]
+        imgs_re = r"\(open-url: .*\)"
+        imgs = [x[10:] for x in re.findall(imgs_re, passage.text)]
+
+        # Remove used matches
+        text = re.sub(keys_re, "", text)
+        text = re.sub(imgs_re, "", text)
+
+        # Prepare states' names
+        state_intro = etree.Element('state')
+        state_intro_name = 'State' + name + 'Intro'
+
+        state_menu = etree.Element('state')
+        state_menu_name = 'State' + name + 'Menu'
+
+        # If name==Start update Start state
+        if name == 'Start':
+            start_tr.attrib['next'] = state_intro_name
+            start_tr.attrib['no_stop'] = 'true'
+            start_tr.text = ""
+
+
+        # Process imgs
+        for img in imgs:
+            text += " ===" + img + "=== "
+
+        # Prepare states
+        state_intro.attrib['name'] = state_intro_name
+        state_menu.attrib['name'] = state_menu_name
+
+        # Prepare transitions
+        tr = etree.Element('transition')
+        tr.attrib['input'] = "*"
+        tr.attrib['next'] = state_menu_name
+        tr.attrib["pending_keyboard"] = ','.join(keys)
+        tr.text = text
+        state_intro.append(tr)
+
+        # Process keys
+        for key in keys:
+            tr = etree.Element('transition')
+            tr.attrib["no_stop"] = "true"
+            tr.attrib["input"] = key
+            tr.attrib["next"] = get_state_intro_name(key)
+            state_menu.append(tr)
+
+        res_xml.append(state_intro)
+        res_xml.append(state_menu)
+
+    start_state.append(start_tr)
+
+    #res = '<states><state name="Start"><transition input="start" next="start">It works better!</transition></state></states>'
+    res = tostring(res_xml)
     return res
 
 
