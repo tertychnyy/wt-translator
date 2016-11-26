35795d4e81a77543a10b9293b857f51602eb5dde
tertychnyy <itertychnyy@gmail.com>
2016-11-27 01:16:04 +0200

    added /go/start method

--- manager.py
+++ manager.py
@@ -5,7 +5,7 @@ from flask_script import Manager
 
 from wt import dashboard, adapter
 
-manager = Manager(dashboard.create_app(config_name="development"))
+manager = Manager(adapter.create_app(config_name="development"))
 
 if __name__ == '__main__':
     manager.run()
