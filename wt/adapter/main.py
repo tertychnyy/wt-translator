3f92aa991aadfb0a83337e96d033fd90dd7fc6eb
tertychnyy <itertychnyy@gmail.com>
2016-11-27 00:48:34 +0200

    added first adapter methods

--- wt/adapter/main.py
+++ wt/adapter/main.py
@@ -3,11 +3,40 @@
 API для SmartCat
 """
 from flask import Blueprint
+from flask import request
 
 from wt.adapter import route
+from wt.adapter.scripts import check_user, new_session
+
 bp = Blueprint('main', __name__)
 
 
 @route(bp, '/test', methods=['GET'])
 def test_page():
+    return 'success', 200
+
+
+@route(bp, '/start', methods=['GET'])
+def start_bot():
+    id_ = request.args.get('id', None)
+
+    # Регистрирует игрока
+    player_id = check_user(id_, "skype")
+
+    # Стартует новую сессию
+    session_id = new_session("eced7582-4acd-46ec-9c78-42df56175dc1", player_id)
+    # Устанавливает
+    # Обнуляет параметры(?)
+    return 'success', 200
+
+
+@route(bp, '/good', methods=['GET'])
+def good_bot():
+    id_ = request.args.get('id', None)
+    return 'success', 200
+
+
+@route(bp, '/bad', methods=['GET'])
+def bad_bot():
+    id_ = request.args.get('id', None)
     return 'success', 200
---
+++ wt/adapter/scripts.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def check_user(id_, channel):
+    """
+    http://localhost:3000/players/create?name=Super User&channel=skype&uid=22211
+    :param id_:
+    :param channel:
+    :return:
+    """
+    url = "https://walkie-talkie-api.herokuapp.com/players/create"
+    params = dict()
+    params["channel"] = channel
+    params["uid"] = id_
+
+    res = requests.get(url, params=params)
+    data = res.json()
+
+    return data["id"]
+
+
+def new_session(game_id, player_id):
+    """
+    curl -X "GET" "http://localhost:3000/games/ff3f5cab-c3d1-4648-a8d2-2d8ad97976e0/start?player_id=029f98b8-37a1-4e45-94a2-18ca3b4ce513"
+    :param game_id:
+    :param player_id:
+    :return:
+    """
+    url = "https://walkie-talkie-api.herokuapp.com/games/{game_id}/start".format(game_id=game_id)
+    params = dict()
+    params["player_id"] = player_id
+
+    res = requests.get(url, params=params)
+    data = res.json()
+
+    return data["id"]
